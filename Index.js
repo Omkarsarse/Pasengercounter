4aa4b6dd9a318c5b76762a2fa9ce685b93b1ae03
Omkarsarse <106108000+Omkarsarse@users.noreply.github.com>
2023-11-06 16:25:37 +0530

    Uploaded all files

---
+++ Index.js
@@ -0,0 +1,24 @@
+﻿//document.getElementById("count").innerText = 5
+//var welcome = document.getElementById("welcome")
+//var name = "Mr. Omkar"
+//var greetings = "Hey , Welcome "
+//welcome.textContent = greetings + name
+//welcome.textContent += "😊😶‍🌫️😶‍🌫️"
+var Incremntlcount = 0
+var saveel = document.getElementById("save-el")
+var savebtn = document.getElementById("save-btn")
+var tcounts = document.getElementById("tcount")
+var count = document.getElementById("count")
+
+function increment() {
+    Incremntlcount += 1
+    count.textContent = Incremntlcount 
+    savebtn.disabled = false;
+}
+function savecount()
+{
+    saveel.textContent += " "+ Incremntlcount + " - "
+    Incremntlcount = 0
+    count.textContent = Incremntlcount
+    savebtn.disabled = true;
+}
